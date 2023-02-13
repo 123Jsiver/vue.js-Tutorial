f8584926921445920ee19d00a368f2dfe4ac7fce
Jesse Siver <123JSiver@frhsd.com>
2023-02-13 10:59:14 -0500

    Button stff added

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    //data, functions
+    // template: '<h2> I am the template </h2>'
+    data() {
+        return{
+            title: 'Among Us',
+            author: 'Brandon Lu',
+            age: 45
+        }
+    },
+    methods: {
+        
+    }
+});
+
+app.mount('#app')
