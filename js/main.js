33b472bc18eb5bfb1d195393de8f8880570704b8
Jesse Siver <123JSiver@frhsd.com>
2023-02-15 08:27:29 -0500

    Vue Directives

--- js/main.js
+++ js/main.js
@@ -3,14 +3,23 @@ const app = Vue.createApp({
     // template: '<h2> I am the template </h2>'
     data() {
         return{
+            showBooks: true,
             title: 'Among Us',
             author: 'Brandon Lu',
             age: 45
         }
     },
     methods: {
-        
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
     }
+//     methods: {
+//         changeTitle(title){
+//         // this.title = 'Words of Randiance'
+//         this.title = title
+//     }
+// }
 });
 
 app.mount('#app')
