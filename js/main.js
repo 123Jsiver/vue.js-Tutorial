2e963a4fc65c5aecfb7cd0830f425147c965a326
Jesse Siver <123JSiver@frhsd.com>
2023-02-16 09:47:45 -0500

    mouse methods and binding

--- js/main.js
+++ js/main.js
@@ -3,17 +3,47 @@ const app = Vue.createApp({
     // template: '<h2> I am the template </h2>'
     data() {
         return{
+            url:"https://www.tradeinn.com/techinn/en/neca-teenage-mutant-ninja-turtles-raphael-18-cm-figure/137792031/p?utm_source=google_products&utm_medium=merchant&id_producte=11773862&country=us",
             showBooks: true,
-            title: 'Among Us',
-            author: 'Brandon Lu',
-            age: 45
+            books: [
+                {title: 'IS THAT LITERATURE?', author: 'Mr. Vona', img: ''},
+                {title: 'How To Be An Idiot', author: 'Christopher John Sandor', img: ''},
+                {title: 'Lil Tokyo Moment', author: 'Brandon Lu', img: ''}
+            ]
+
+            // title: 'Among Us',
+            // author: 'Brandon Lu',
+            // age: 45,
+            // x: 0,
+            // y: 0
         }
     },
     methods: {
         toggleShowBooks() {
-            this.showBooks = !this.showBooks
-        }
+                    this.showBooks = !this.showBooks
+                }
     }
+
+
+
+    // For mouse events
+
+    // methods: {
+    //     handleEvent(e, data) {
+    //         console.log(e, e.type)
+    //         if(data){
+    //             console.log(data)
+    //         }
+    //     },
+    //     handleMousemove(e) {
+    //         this.x = e.offsetX
+    //         this.y = e.offsetY
+    //     }
+    // }
+
+
+    // for changing titles
+
 //     methods: {
 //         changeTitle(title){
 //         // this.title = 'Words of Randiance'
